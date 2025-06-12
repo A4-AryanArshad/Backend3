ccf5aa5d675401edd9506389dc6d54d861ec8d1e
Aryan Arshad <126335847+A4-AryanArshad@users.noreply.github.com>
2025-06-12 20:31:36 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(cors());
 
 
-mongoose.connect('mongodb+srv://nihaarshad5:2021cs613@cluster0.o8bu9nt.mongodb.net/', {
+mongoose.connect('mongodb+srv://zainmanzoor2003:react-native-first-app123@react-native-first-app.d3vxvgp.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
